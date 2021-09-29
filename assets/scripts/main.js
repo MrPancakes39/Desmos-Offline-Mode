792a726692b2ef0a498bfa586d318958be2314f1
MrPancakes39 <salmannhassan39@gmail.com>
2021-09-29 23:00:21 +0300

    Added Screenshot Icon
    - Added FileDownloader Class.
    - Moved some styling from js to css.
    - Now when you click on the background in rename graph modal it closes.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,14 +2,17 @@
 // saveBtn.enable = (e) => e.removeClass("dcg-btn-green").addClass("dcg-disabled");
 // saveBtn.disable = (e) => e.removeClass("dcg-disabled").addClass("dcg-btn-green");
 
+const fDown = new FileDownloader();
+
 function setupDOM() {
     $(".dcg-config-name")[0].innerText = "Untitled Graph";
-    $(".dcg-header.dcg-secure-header.dcg-header-desktop")[0].style["background-color"] = "#2a2a2a";
-    $(".align-left-container")[0].style["width"] = "auto";
-    $(".title-div")[0].style["padding-right"] = "0px";
+    $(".dcg-header.dcg-secure-header.dcg-header-desktop").css("background-color", "#2a2a2a");
     $(".align-left-container")
         .append(`<span class="dcg-if-user open-btn-container"><span role="button" tooltip="Open File    (ctrl+o)" class="dcg-action-open tooltip-offset dcg-btn-red  " ontap="" original-title="">Open</span></span>`)
         .append(`<span class="dcg-if-user save-btn-container"><span role="button" tooltip="Save Changes (ctrl+s)" class="dcg-action-save tooltip-offset dcg-btn-green" ontap="" original-title="">Save</span></span>`);
+    $(".align-right-container")
+        .prepend(`<div class="dcg-tooltip-hit-area-container" handleevent="true" ontap=""><svg class="dcg-icon-screenshot" aria-label="Take a Screenshot" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="3.2"/><path d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"/></svg></div>`);
+    addScreenshotTooltip();
 }
 
 function eventHandlers() {
@@ -21,7 +24,8 @@ function eventHandlers() {
     $(".dcg-config-name").click(() => {
         let txt = $(".dcg-config-name").text();
         let title = (txt != "Untitled Graph") ? txt : "";
-        $("body").append(renameGraphAlert(title));
+        $("body").append(renameAlert(title));
+        $(".dcg-modal-background").click(() => $(".dcg-modal-background").parent().remove());
         $(".dcg-icon-remove-custom").click(() => $(".dcg-alert-container").remove());
         $(".title-save").click(() => {
             let txt = $(".title-input").val();
@@ -30,7 +34,20 @@ function eventHandlers() {
             $(".dcg-icon-remove-custom").click();
         });
         $(".title-input").select();
+    });
+    $(".dcg-icon-screenshot")
+        .click(() => {
+            fDown.saveImage(Calc.screenshot());
+            $(".dcg-tooltip-mount-pt-screenshot").hide();
     })
+        .hover(
+            () => setTimeout(() => $(".dcg-tooltip-mount-pt-screenshot").show(), 500),
+            () => setTimeout(() => {
+                if (!($(".dcg-tooltip-mount-pt-screenshot").is(":hover")))
+                    $(".dcg-tooltip-mount-pt-screenshot").hide();
+            }, 150)
+        );
+    $(".dcg-tooltip-mount-pt-screenshot").mouseleave(() => $(".dcg-tooltip-mount-pt-screenshot").hide());
 }
 
 function customAlert(title, msg) {
@@ -52,7 +69,7 @@ function customAlert(title, msg) {
     `;
 }
 
-function renameGraphAlert(title) {
+function renameAlert(title) {
     return `
     <div class="dcg-alert-container">
         <div class="dcg-modal-background"></div>
@@ -71,4 +88,20 @@ function renameGraphAlert(title) {
         </div>
     </div>
     `;
+}
+
+function addScreenshotTooltip() {
+    $("body").append(
+        `<div class="dcg-tooltip-mount-pt-screenshot" style="display: none;">
+            <div class="dcg-tooltip-positioning-container" style="top:5px;left:369px;width:36px;height:36px">
+                <div class="dcg-tooltip-message-container" style="top:100%;right:0px;margin-top:5px;text-align:right">
+                    <div class="dcg-tooltip-message" style="background:#000;cursor:default">Take a Screenshot</div>
+                </div>
+                <div class="dcg-tooltip-arrow" style="top:100%;left:50%;border:5px solid transparent;border-color:transparent transparent #000 transparent;margin-top:-5px;margin-left:-5px"></div>
+            </div>
+        </div>`
+    );
+    let temp = $(".dcg-tooltip-mount-pt-screenshot .dcg-tooltip-positioning-container");
+    temp[0].style["top"] = "-2.9px";
+    temp[0].style["left"] = `${$(".dcg-icon-screenshot").offset()["left"] - 5}px`;
 }
