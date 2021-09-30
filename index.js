9f5367d683ce4a259ee62e1987805dd6f3aedf43
MrPancakes39 <salmannhassan39@gmail.com>
2021-09-30 20:52:00 +0300

    Added a simple express server

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on http://127.0.0.1:${PORT}/`));
+app.use(express.static("app"));
