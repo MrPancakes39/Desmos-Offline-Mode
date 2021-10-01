151a37a4e15cb74959d0d080b54e1a053ab85edd
MrPancakes39 <salmannhassan39@gmail.com>
2021-10-01 19:31:40 +0300

    Created a Menu for the App

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
+
+const menu_template = require("./menu-template");
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1280,
         height: 720,
-        icon: path.join(__dirname, "app", "favicon.ico")
+        icon: path.join(__dirname, "app", "favicon.ico"),
+        webPreferences: {
+            sandbox: true
+        }
     });
+
+    const menu = Menu.buildFromTemplate(menu_template);
+    Menu.setApplicationMenu(menu);
+
+    win.maximize();
     win.loadFile(path.join(__dirname, "app", "index.html"));
+    win.on("ready-to-show", () => win.show());
 }
 
 app.whenReady().then(() => {
---
+++ menu-template.js
@@ -0,0 +1,39 @@
+const { shell } = require("electron");
+
+const isMac = process.platform === "darwin";
+const isLinux = process.platform === "linux";
+
+module.exports = [{
+        label: "File",
+        submenu: [
+            { label: "Open File", accelerator: "CmdOrCtrl+O" },
+            { label: "Save", accelerator: "CmdOrCtrl+S" },
+            { label: "Save as", accelerator: "CmdOrCtrl+Shift+S" },
+            { type: "separator" },
+            {
+                label: "Exit",
+                ...(isMac ? { role: "close" } : { role: "quit" }),
+                accelerator: isMac ? "Cmd+W" : (isLinux ? "Ctrl+Q" : "Alt+F4")
+            }
+        ]
+    },
+    {
+        label: "View",
+        submenu: [
+            { label: "Reset zoom", role: "resetZoom" },
+            { role: "zoomIn" },
+            { role: "zoomOut" },
+            { role: "togglefullscreen" },
+            { type: "separator" },
+            { role: "toggleDevTools" }
+        ]
+    }, {
+        role: "help",
+        submenu: [
+            { label: "Documentation" },
+            { label: "Report Issue", click: async () => await shell.openExternal("https://github.com/MrPancakes39/Desmos-Offline-Mode/issues") },
+            { type: "separator" },
+            { label: "About Desmos Offline Mode", role: "about" }
+        ]
+    }
+]
