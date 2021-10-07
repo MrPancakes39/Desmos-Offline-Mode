9feec18044459cc6b96c4104cc574a63611bbdba
MrPancakes39 <salmannhassan39@gmail.com>
2021-10-07 18:17:11 +0300

    Removed API to conform with Desmos Web Version

--- app/assets/scripts/fix_calc.js
+++ app/assets/scripts/fix_calc.js
@@ -81,76 +81,37 @@ function fixCalc() {
     // Fix Calc Settings
     Calc.default_settings = default_settings;
     Calc.updateSettings(default_settings);
-    Calc.setDefaultState(Calc.getState());
     Calc.newRandomSeed();
 
-    Calc._getExpressions = () => {
-        return Calc.getState({
-            stripDefaults: !1
-        }).expressions.list.map(Calc._santizer.sanitizeItem);
+    Calc.lastSelectedId = () => Calc._calc.controller.__lastSelectedId;
+
+    Calc.getSelectedItem = () => {
+        let e = Calc._calc.controller.getSelectedItem();
+        if (typeof e === "undefined")
+            return;
+        return Calc._santizer.sanitizeItem(e);
     }
 
-    Calc.getExpressions = () => {
-        return Calc.getState({
-            stripDefaults: !1
-        }).expressions.list.map((e) => {
-            let t = (e.type === "expression" || e.type === "table") ? Calc._santizer.sanitizeItem(e) : e;
-            t.folderId = e.folderId;
-            return t;
-        });
+    Calc.colorRotation = [Desmos.Colors.RED, Desmos.Colors.BLUE, Desmos.Colors.GREEN, Desmos.Colors.PURPLE, Desmos.Colors.BLACK];
+
+    Calc.setNextColor = (color) => {
+        let validColors = Calc.colorRotation;
+        if (!validColors.includes(color)) {
+            console.error(`${color} is not a valid color.`);
+            return;
+        }
+        let t = Calc._calc.controller;
+        let id = validColors.indexOf(color);
+        t.listModel.colorIdx = id;
     }
 
-    Calc._setExpression = (e) => {
-        let self = Calc._calc;
-        let d = Calc._santizer;
-        let t = self.controller,
-            n = t.getItemModel(e.id),
-            r = n && n.type,
-            i = d.validateItem(e, r, self.controller);
-        if (i)
-            if (r) self.controller.dispatch({
-                type: "set-expression-properties-from-api",
-                id: i.id,
-                properties: i
-            });
-            else {
-                if ("image" === i.type) return;
-                self.controller.dispatch({
-                    type: "add-item-to-end-from-api",
-                    state: i
-                })
-            }
-    }
-
-    Calc.setExpression = (e) => {
-        let self = Calc._calc;
-        if (e.type === "text" || e.type === "image" || e.type === "folder") {
-            self.controller.dispatch({
-                type: "add-item-to-end-from-api",
-                state: e
-            });
-        } else Calc._setExpression(e);
-    }
-
-    Calc._setExpressions = (e) => e.forEach(exp => Calc._setExpression(exp));
-    Calc.setExpressions = (e) => e.forEach(exp => Calc.setExpression(exp));
-
-    Calc.setStateExpression = (e) => {
-        let state = Calc.getState();
-        let l = state.expressions.list;
-        for (let i = 0; i < l.length; i++)
-            if (l[i].id === e.id) {
-                l[i] = { ...e };
-                break;
-            }
-        state.expressions.list = l;
-        Calc.setState(state);
-    }
-
-    Calc.setStateExpressions = (e) => {
-        let state = Calc.getState();
-        state.expressions.list = e;
-        Calc.setState(state);
+    Calc.setSelectedItemColor = (color) => {
+        let e = Calc.getSelectedItem();
+        if (typeof e === "undefined")
+            return;
+        e.color = color;
+        if (e.type === "expression" || e.type === "table")
+            Calc.setExpression(e);
     }
 
     console.log("[fix_calc] calc api fixed!");
