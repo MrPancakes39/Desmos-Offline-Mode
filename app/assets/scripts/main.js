64e3d020bb40ec95d33b35ebb18e336478455e8d
MrPancakes39 <salmannhassan39@gmail.com>
2021-10-25 21:12:20 +0300

    Modified New Graph Option
    - Show "New Graph Created" Toast.
    - If there isn't any Unsaved Changes Create New Graph Immediately.

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -176,8 +176,11 @@ function addAlert(alert, type) {
                 Calc.setBlank();
                 Calc.newRandomSeed();
                 Calc._calc.controller._hasUnsavedChanges = true;
+                Calc._calc.controller.dispatch({ type: "toast/show", toast: { message: "New Graph Created", hideAfter: 4e3 } });
                 nodeAPI.send("newFile", "[main] new file");
-            })
+            });
+            if (!Calc._calc.controller._hasUnsavedChanges)
+                $(".dcg-btn-red.dcg-action-delete").click();
             break;
 
         case "about":
