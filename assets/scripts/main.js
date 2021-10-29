7c86ca9856c193b9f41385fe1ea561aa6210abd8
MrPancakes39 <salmannhassan39@gmail.com>
2021-10-29 18:36:43 +0300

    Refactored main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -65,11 +65,9 @@ function eventHandlers() {
     $(".dcg-icon-web").click(() => Calc._calc.controller._openOnWeb());
 
     $(".dcg-if-user.save-btn-container").click(() => {
-        let title = $(".dcg-config-name").text();
-        let state = Calc.getState();
-        let colorIdx = Calc._calc.controller.listModel.colorIdx;
-        let stringify = JSON.stringify({ title, state, colorIdx });
-        fDown.saveStrings(stringify.split("\n"), title, "desmos");
+        let config = makeConfig();
+        let stringify = JSON.stringify(config);
+        fDown.saveStrings(stringify.split("\n"), config["title"], "desmos");
     });
 
     $(".dcg-action-open.tooltip-offset.dcg-btn-red").click(() => $(".open-btn-container>input").click());
@@ -88,11 +86,6 @@ function eventHandlers() {
                 let data = JSON.parse(fr.result);
                 $(".dcg-config-name").text(data["title"]);
                 Calc.setState(data["state"]);
-
-                let id = data["colorIdx"];
-                let nextColor = Calc.colorRotation[id] || Desmos.Colors.RED;
-                Calc.setNextColor(nextColor);
-
             };
             fr.readAsText(file);
         }
@@ -104,68 +97,11 @@ function eventHandlers() {
     console.log("[main] event handlers setup done!");
 }
 
-function customAlert(title, msg) {
-    return `
-    <div class="dcg-alert-container">
-        <div class="dcg-modal-background"></div>
-        <div class="dcg-box-modal">
-            <span role="link" tabindex="0" aria-label="Close Dialog" class="dcg-close-modal" ontap="">
-                <i class="dcg-icon-remove-custom"></i>
-            </span>
-            <div class="alert-dialog">
-                <h1>${title}</h1>
-                <div class="alert-content">
-                    <p>${msg}</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
-}
-
-function renameAlert(title) {
-    return `
-    <div class="dcg-alert-container">
-        <div class="dcg-modal-background"></div>
-        <div class="dcg-box-modal">
-            <span role="link" tabindex="0" aria-label="Close Dialog" class="dcg-close-modal" ontap="">
-                <i class="dcg-icon-remove-custom"></i>
-            </span>
-            <div class="alert-dialog">
-                <h1>Rename This Graph</h1>
-                <div class="alert-content">
-                    <p class="graph-title">Title</p>
-                    <input class="title-input" name="title" placeholder="Untitled Graph" value="${title}" maxlength="140" tabindex="0" autofocus="true">
-                    <button type="submit" class="title-save">Save</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
-}
-
-function confirmAlert() {
-    return `
-    <div class="dcg-alert-container">
-        <div class="dcg-modal-background"></div>
-        <div class="dcg-box-modal">
-            <span role="link" tabindex="0" aria-label="Close Dialog" class="dcg-close-modal" ontap="">
-                <i class="dcg-icon-remove-custom"></i>
-            </span>
-            <div class="alert-dialog">
-                <h1>Create New Graph</h1>
-                <div class="alert-content">
-                    <p>Are you sure you want to create a new graph?
-                    This graph will be lost</p>
-                    <div class="alert-buttons">
-                        <button type="submit" class="dcg-dark-gray-link">Cancel</button>
-                        <button type="submit" class="dcg-btn-red dcg-action-delete">New</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
+function makeConfig() {
+    let config = {};
+    config["title"] = $(".dcg-config-name").text();
+    config["state"] = Calc.getState();
+    return config;
 }
 
 function addAlert(alert, type) {
