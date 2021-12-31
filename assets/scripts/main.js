781b4966c431c6a11835530c1dbbb00c4213a612
Salman Hasan <salmannhassan39@gmail.com>
2021-12-31 14:34:56 +0200

    Removed Header background-color from main.css

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,19 @@
-define("calc/init", ["calc/fix_calc"], function (fixCalc) {
+define("calc/setup_dom", ["jquery"], function ($) {
+    return function () {
+        $(".dcg-header.dcg-secure-header.dcg-header-desktop").css(
+            "background-color",
+            "#2a2a2a"
+        );
+        console.log("[main] dom setup done!");
+    };
+});
+
+define("calc/init", ["calc/fix_calc", "calc/setup_dom"], function (
+    fixCalc,
+    setupDOM
+) {
     return function () {
         fixCalc();
+        setupDOM();
     };
 });
