b886d0c92d14393f98050e34a40d9b2ca6bd44e6
MrPancakes39 <salmannhassan39@gmail.com>
2022-11-29 18:39:29 +0200

    Added Basic Calculator Build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,26 @@
-define("calc/app", [], function () {
-    console.log("Hello");
+"use strict";
+
+// defines window.IS_BROWSER constant
+Object.defineProperty(window, "IS_BROWSER", {
+    value: window.__TAURI__ === undefined,
+    writable: false,
+});
+
+define("calc/build", [
+    "main/instantiate-top-level-components",
+    "text!example_graphs",
+], function (init_components, example_graphs) {
+    let seed = crypto.randomUUID().split("-").join("");
+    // temporary exposer
+    window.descont = init_components.default({
+        seed: seed,
+        exampleGraphsRaw: example_graphs,
+        calcOptions: { pasteGraphLink: !0 },
+    });
+
+    window.Calc = descont.Calc;
+});
+
+define("calc/app", ["api/calculator", "calc/build"], function () {
+    console.log(window.Calc);
 });
