15a8f3f484e246fc0843ce45e8903c70a3823a55
MrPancakes39 <salmannhassan39@gmail.com>
2023-01-12 18:58:07 +0200

    Added htm lib with a test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,31 @@
 "use strict";
 
+define("calc/play", ["htm", "dcgview"], function (htm, dcgview) {
+    // for debugging
+    window.dcgview = dcgview;
+    // setup
+    const html = htm.bind(dcgview.createElement);
+    const { Tooltip } = require("shared-components/tooltip");
+    const cst = dcgview.const;
+
+    class Button extends dcgview.Class {
+        template() {
+            return html`
+            <div role=${cst("button")} class=${cst("hello-btn")}>
+                <${Tooltip}>
+                    <span>${cst("Hello")}</span>
+                </${Tooltip}>
+            </div>
+            `;
+        }
+    }
+
+    const domELT = document.querySelector("#dcg-header-container");
+    console.log(Button, domELT);
+    dcgview.mountToNode(Button, domELT);
+});
+
 define("calc/app", ["calc/load_api"], function () {
-    console.log(window.Calc);
+    // console.log(window.Calc);
+    require("calc/play");
 });
