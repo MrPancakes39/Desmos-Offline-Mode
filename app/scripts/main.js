894873238af797178bb051cacc42a03085035003
MrPancakes39 <salmannhassan39@gmail.com>
2023-01-15 19:35:42 +0200

    Added basic containers in header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,25 @@
 "use strict";
 
-define("calc/app", ["calc/load_api"], function () {
+define("calc/header", ["htm", "dcgview"], function (htm, n) {
+    const html = htm.bind(n.createElement);
+    window.n = n;
+
+    class Header extends n.Class {
+        template() {
+            return html`
+                <div class=${n.const("dcg-header")}>
+                    <div class=${n.const("align-left-container")}></div>
+                    <div class=${n.const("align-center-container")}></div>
+                    <div class=${n.const("align-right-container")}></div>
+                </div>
+            `;
+        }
+    }
+
+    const headerContainer = document.querySelector("#dcg-header-container");
+    n.mountToNode(Header, headerContainer);
+});
+
+define("calc/app", ["calc/load_api", "calc/header"], function () {
     console.log(window.Calc);
 });
