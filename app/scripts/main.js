5aad8f312d8f4d0adeb77561730d71ada016078e
MrPancakes39 <salmannhassan39@gmail.com>
2023-01-15 13:22:46 +0200

    Added help icon as a test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,23 +10,41 @@ define("calc/play", ["htm", "dcgview", "touchtracking"], function (
     // setup
     const html = htm.bind(dcgview.createElement);
     const { Tooltip } = require("shared-components/tooltip");
+    const { HeaderIcon } = require("calculator-shell/header-icon");
     const cst = dcgview.const;
 
     class Button extends dcgview.Class {
         template() {
-            return html`
-            <div role=${cst("button")}
-                class=${cst("hello-btn")}
-                onClick=${() => alert("Hello")}
+            let x = html`
+            <div style=${() => "display: flex;flex-direction: row;"}
                 didMount=${function (elt) {
                     touch.monitor(elt);
                 }}
             >
-                <${Tooltip} tooltip=${cst("Hello")} gravity=${cst("se")}>
-                    <span>${cst("Hello")}</span>
-                </${Tooltip}>
+                <div role=${cst("button")}
+                    class=${cst("hello-btn")}
+                    onClick=${() => alert("Hello")}
+                >
+                    <${Tooltip} tooltip=${cst("Hello")} gravity=${cst("se")}>
+                        <span>${cst("Hello")}</span>
+                    </${Tooltip}>
+                </div>
+                <${HeaderIcon}
+                    controller=${() => descont.headerController}
+                    label=${() => Calc.controller.s("account-shell-label-help")}
+                    tooltip=${() =>
+                        Calc.controller.s("account-shell-label-help")}
+                    icon=${cst("dcg-icon-question-sign")}
+                    name=${cst("help")}
+                    onTap=${function (t) {
+                        console.log(t);
+                    }}
+                >
+                </${HeaderIcon}>
             </div>
             `;
+            console.log(x);
+            return x;
         }
     }
 
