6b4bd32d79b4cd0845dc2e05d585f891eb78ee14
MrPancakes39 <salmannhassan39@gmail.com>
2023-01-15 14:24:18 +0200

    Refactored load_api

--- app/scripts/load_api.js
+++ app/scripts/load_api.js
@@ -1,67 +1,56 @@
-define("calc/private_props", ["jquery"], function ($) {
-    return {
-        colorRotation: [
-            Desmos.Colors.RED,
-            Desmos.Colors.BLUE,
-            Desmos.Colors.GREEN,
-            Desmos.Colors.PURPLE,
-            Desmos.Colors.BLACK,
-        ],
+define("calc/private/toggle-options", [], function () {
+    return (item) => {
+        let t = Calc._calc.controller;
+        let item_modal = t.getItemModel(item.id);
+        let item_type = item.sliderExists ? "slider" : item.type;
+        if (
+            item_type === "expression" ||
+            item_type === "image" ||
+            item_type === "slider"
+        ) {
+            t.dispatch({
+                type: "toggle-item-settings-menu",
+                menu: {
+                    type: item_type,
+                    model: item_modal,
+                    focusFirstOption: !1,
+                },
+            });
+        } else {
+            throw new Error(`"${item_type}" type doesn't have menu options.`);
+        }
+    };
+});
 
-        toggleOptions: (item) => {
-            let t = Calc._calc.controller;
-            let item_modal = t.getItemModel(item.id);
-            let item_type = item.sliderExists ? "slider" : item.type;
-            if (
-                item_type === "expression" ||
-                item_type === "image" ||
-                item_type === "slider"
-            ) {
-                t.dispatch({
-                    type: "toggle-item-settings-menu",
-                    menu: {
-                        type: item_type,
-                        model: item_modal,
-                        focusFirstOption: !1,
-                    },
-                });
-            } else {
-                throw new Error(
-                    `"${item_type}" type doesn't have menu options.`
-                );
-            }
-        },
-
-        addItem: (type) => {
-            const validTypes = [
-                "expression",
-                "table",
-                "note",
-                "text",
-                "folder",
-                "image",
-                "simulation",
-            ];
-            if (!validTypes.includes(type)) {
-                throw new Error(`${type} is not a valid type.`);
-            }
-            let e = Calc._calc.controller;
-            if (type !== "image") {
-                let t = type === "note" ? "new-text" : `new-${type}`;
-                e.dispatch({ type: t });
-            } else {
-                const imageInput = $(
-                    `<input type="file" accept="image/*" style="display: none">`
-                );
-                imageInput
-                    .on("change", () => {
-                        const fileList = imageInput.prop("files");
-                        e.dispatch({ type: "new-images", files: fileList });
-                    })
-                    .click();
-            }
-        },
-        sanitizer: require("graphing-calc/api/sanitize-expression"),
+define("calc/private/add-item", ["jquery"], function ($) {
+    return (type) => {
+        const validTypes = [
+            "expression",
+            "table",
+            "note",
+            "text",
+            "folder",
+            "image",
+            "simulation",
+        ];
+        if (!validTypes.includes(type)) {
+            throw new Error(`${type} is not a valid type.`);
+        }
+        let e = Calc._calc.controller;
+        if (type !== "image") {
+            let t = type === "note" ? "new-text" : `new-${type}`;
+            e.dispatch({ type: t });
+        } else {
+            const imageInput = $(
+                `<input type="file" accept="image/*" style="display: none">`
+            );
+            imageInput
+                .on("change", () => {
+                    const fileList = imageInput.prop("files");
+                    e.dispatch({ type: "new-images", files: fileList });
+                })
+                .click();
+        }
     };
 });
 
@@ -69,21 +58,29 @@ define("calc/load_api", [
     "api/calculator",
     "main/instantiate-top-level-components",
     "text!example_graphs",
-    "calc/private_props",
-], function (_, init_components, example_graphs, props) {
-    let seed = crypto.randomUUID().split("-").join("");
+], function (_, init_components, example_graphs) {
+    const seed = crypto.randomUUID().split("-").join("");
     // temporary exposer
     window.descont = init_components.default({
         seed: seed,
         exampleGraphsRaw: example_graphs,
         calcOptions: { pasteGraphLink: !0 },
     });
-    let { Calc } = descont;
+    const { Calc } = descont;
+
+    const sanitizer = require("graphing-calc/api/sanitize-expression");
+    const colorRotation = [
+        Desmos.Colors.RED,
+        Desmos.Colors.BLUE,
+        Desmos.Colors.GREEN,
+        Desmos.Colors.PURPLE,
+        Desmos.Colors.BLACK,
+    ];
 
     Calc.getSelectedItem = () => {
         let e = Calc._calc.controller.getSelectedItem();
         if (e) {
-            let i = props.sanitizer.sanitizeItem(e);
+            let i = sanitizer.sanitizeItem(e);
             if (i.type === "expression") {
                 delete i.fill;
                 delete i.lines;
@@ -101,7 +98,7 @@ define("calc/load_api", [
     Calc.getNextColor = () => {
         let t = Calc._calc.controller;
         let index = t.listModel.colorIdx;
-        return props.colorRotation[index];
+        return colorRotation[index];
     };
 
     Calc.printNextColor = () => {
@@ -111,11 +108,11 @@ define("calc/load_api", [
     };
 
     Calc.setNextColor = (color) => {
-        if (!props.colorRotation.includes(color)) {
+        if (!colorRotation.includes(color)) {
             throw new Error(`${color} is not a valid color.`);
         }
         let t = Calc._calc.controller;
-        let id = props.colorRotation.indexOf(color);
+        let id = colorRotation.indexOf(color);
         t.listModel.colorIdx = id;
     };
 
