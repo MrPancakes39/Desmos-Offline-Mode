5109eaa2ed1e8f3f537d351b292a3200fc5cce6b
MrPancakes39 <selenahasan39@proton.me>
2023-04-20 22:31:23 +0300

    Added Fragile type

--- src/globals/Desmos.ts
+++ src/globals/Desmos.ts
@@ -2,6 +2,53 @@ import "desmos";
 
 type DesmosType = typeof Desmos & {
   version: string;
+  Private: {
+    Fragile: FragileType;
+  };
+};
+
+type FragileType = {
+  DCGView: DCGViewModule;
+  PromptSliderView: any;
+  Checkbox: typeof CheckboxComponent;
+  SegmentedControl: typeof SegmentedControlComponent;
+  MathquillView: typeof MathQuillViewComponent & {
+    // static abstract getFocusedMathquill()
+    getFocusedMathquill: () => MathQuillField;
+  };
+  InlineMathInputView: typeof InlineMathInputViewComponent;
+  StaticMathquillView: typeof DStaticMathquillViewComponent;
+  Tooltip: typeof TooltipComponent;
+  ExpressionOptionsMenuView: {
+    prototype: {
+      getSections: {
+        apply: (m: { model: ItemModel }) => Section[];
+      };
+    };
+  };
+  evaluateLatex: (s: string, isDegreeMode: boolean) => number;
+  Keys: {
+    lookup: (e: KeyboardEvent) => string;
+    lookupChar: (e: KeyboardEvent) => string;
+    isUndo: (e: KeyboardEvent) => boolean;
+    isRedo: (e: KeyboardEvent) => boolean;
+    isHelp: (e: KeyboardEvent) => boolean;
+  };
+  jQuery: any;
+  getQueryParams: () => Record<string, string | true>;
+  getReconciledExpressionProps: (
+    type: string,
+    model?: ItemModel
+  ) => {
+    points: boolean;
+    lines: boolean;
+    fill: boolean;
+  };
+  List: {
+    removeItemById: (listModel: any, id: string) => void;
+    moveItemsTo: (listModel: any, from: number, to: number, n: number) => void;
+  };
+  currentLanguage: () => string;
 };
 
 export default DesmosType;
