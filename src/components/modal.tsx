dcc41b2832a34dfad18d2ebb49ab4e4eb31c8499
MrPancakes39 <salmannhassan39@gmail.com>
2023-12-02 20:35:07 +0200

    Init Modal Component

---
+++ src/components/modal.tsx
@@ -0,0 +1,36 @@
+import "./modal.less";
+import { Component, jsx } from "../DCGView";
+import { If } from ".";
+import { DesmosOM } from "../globals";
+
+export default class Modal extends Component<{
+  title: string;
+  showModal: boolean;
+}> {
+  template() {
+    return (
+      <If predicate={() => this.props.showModal()}>
+        {() => (
+          <div role="dialog" class="desom-modal-container">
+            <div class="desom-modal-background" onTap=""></div>
+            <div class="desom-modal">
+              <span
+                role="link"
+                tabindex="0"
+                aria-label={DesmosOM.format("shared-button-close-dialog")}
+                class="close-modal"
+                onTap=""
+              >
+                <i class="dcg-icon-remove"></i>
+              </span>
+              <div class="modal-dialog">
+                <h1>{this.props.title()}</h1>
+                <div class="modal-content">{this.children}</div>
+              </div>
+            </div>
+          </div>
+        )}
+      </If>
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Calc, Fragile } from "./globals/window";
 import Header from "./components/headerComponent";
+import Modal from "./components/modal";
 
 function select<E extends Element>(selector: string): E {
   const tmp = document.querySelector<E>(selector);
@@ -12,8 +13,21 @@ function select<E extends Element>(selector: string): E {
 (function App() {
   console.log(Calc); // Sanity Check
 
+  let foo = false;
+
+  setTimeout(() => {
+    foo = true;
+    console.log("It's set");
+    Calc.controller.updateViews();
+  }, 3000);
+
   const HeaderContainer = select<HTMLDivElement>("#dcg-header-container");
   Fragile.DCGView.mountToNode(Header, HeaderContainer, {});
+  const view = Fragile.DCGView.mountToNode(Modal, select<HTMLDivElement>("#dcg-modal-container"), {
+    title: () => "Title Here",
+    showModal: () => foo,
+  });
+  Calc.controller.subscribeToChanges(() => view.update());
 
   // Last in loading
   Calc.focusFirstExpression();
