70f27075515649233aeedfbc7597bb6b4cd70739
MrPancakes39 <salmannhassan39@gmail.com>
2023-12-17 22:49:23 +0200

    Fixed Resizing SlideBar Bug & Added Comments

--- src/components/sidebarComponent/SideBarController.ts
+++ src/components/sidebarComponent/SideBarController.ts
@@ -6,6 +6,7 @@ import SideBarContainer from "./SideBarContainer";
 export class SideBarController {
   unsub: (() => void) | undefined;
   removeEmptySpaceDiv: (() => void) | undefined;
+  removeResizeListener: (() => void) | undefined;
   divContainer: HTMLDivElement | undefined;
   slidingInterior: HTMLDivElement | undefined;
   emptySpaceDiv: HTMLDivElement | undefined;
@@ -26,16 +27,27 @@ export class SideBarController {
     });
     this.unsub = this.dsom.cc.subscribeToChanges(() => view.update());
 
+    // Adds an empty div as first child of expressionTopBar.
+    // This is so when the screen width is 450px the hamburger menu looks like it's in the expression bar.
     const expressionTopBar = select<HTMLDivElement>(".dcg-expression-top-bar");
     const emptySpaceDiv = document.createElement("div");
     emptySpaceDiv.className = "side-bar-empty-space";
     expressionTopBar.prepend(emptySpaceDiv);
     this.removeEmptySpaceDiv = () => expressionTopBar.removeChild(emptySpaceDiv);
+
+    // If the hamburger menu goes away (screen width > 450px) then we should hide the sidebar.
+    const handleResize = () => {
+      const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      if (windowWidth > 450 && this.shouldShow) this.hideSideBar();
+    };
+    window.addEventListener("resize", handleResize);
+    this.removeResizeListener = () => window.removeEventListener("resize", handleResize);
   }
 
   destroy() {
     this.unsub?.();
     this.removeEmptySpaceDiv?.();
+    this.removeResizeListener?.();
     if (this.divContainer) Fragile.DCGView.unmountFromNode(this.divContainer);
   }
 
