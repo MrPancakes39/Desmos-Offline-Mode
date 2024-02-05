c5a65cad3859d00ab95b8195a814c93835a129e3
MrPancakes39 <salmannhassan39@gmail.com>
2024-02-05 13:08:46 +0200

    Removed buttons in header for sidebar

--- src/components/headerComponent/left-container.tsx
+++ src/components/headerComponent/left-container.tsx
@@ -7,37 +7,18 @@ export default class LeftContainer extends Component<{}> {
   template() {
     return (
       <div class="left-container">
-        <Tooltip tooltip={window.DSOM.format("account-shell-text-mygraphs-new-blank-graph")} gravity="s">
-          <div class="center-div">
+        <Tooltip tooltip={""} gravity="s">
+          <div class="center-div sidebar-fix">
             <DCGButton
               color="invisible"
-              onTap={() => window.DSOM.modalController.showModal("new-graph")}
-              class="desom-icons-file-plus desom-icon-inline"
-              aria-label={window.DSOM.format("account-shell-text-mygraphs-new-blank-graph")}
-            ></DCGButton>
+              onTap={() => window.DSOM.sidebarController.toggleSideBar()}
+              class="desom-icon-btn sidebar-menu-btn"
+            >
+              <i class="dcg-icon-hamburger" aria-hidden="true"></i>
+            </DCGButton>
           </div>
         </Tooltip>
         <h1 class="graph-title">Untitled Graph</h1>
-        <Tooltip tooltip={window.DSOM.format("account-shell-button-open-graph")} gravity="s">
-          <div class="center-div">
-            <DCGButton
-              color="primary"
-              onTap={() => console.log("open")}
-              class="dcg-btn-primary desom-icon-btn desom-icons-folder-open"
-              aria-label={window.DSOM.format("account-shell-button-open-graph")}
-            ></DCGButton>
-          </div>
-        </Tooltip>
-        <Tooltip tooltip={window.DSOM.format("account-shell-button-mygraphs-save")} gravity="s">
-          <div class="center-div">
-            <DCGButton
-              color="primary"
-              onTap={() => console.log("save")}
-              class="desom-icon-btn desom-icons-save"
-              aria-label={window.DSOM.format("account-shell-button-mygraphs-save")}
-            ></DCGButton>
-          </div>
-        </Tooltip>
       </div>
     );
   }
