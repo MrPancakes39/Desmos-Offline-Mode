0bdfa9edddcf4ba489c230347c6abecc3f90112c
MrPancakes39 <salmannhassan39@gmail.com>
2024-03-24 22:01:47 +0200

    Added SegmentedControl Component

---
+++ src/components/common/SegmentedControl.tsx
@@ -0,0 +1,40 @@
+// Original File: https://github.com/DesModder/DesModder/blob/main/src/components/SegmentedControl.tsx
+import "./SegmentedControl.less";
+import { DesmosSegmentedControl, Switch } from "../desmosComponents";
+import { Component, jsx } from "#DCGView";
+
+export default class SegmentedControl extends Component<{
+  names: string[];
+  selectedIndex: number;
+  ariaGroupLabel: string;
+  setSelectedIndex: (i: number) => void;
+  allowChange?: boolean;
+}> {
+  template() {
+    return (
+      <Switch key={() => this.props.names().join(";")}>
+        {() => (
+          <DesmosSegmentedControl
+            staticConfig={this.getStaticConfig()}
+            ariaGroupLabel={this.props.ariaGroupLabel()}
+            disabled={() => !this.getChangeAllowed(-1)}
+          />
+        )}
+      </Switch>
+    );
+  }
+
+  getStaticConfig() {
+    return this.props.names().map((name, i) => ({
+      key: name,
+      label: () => name,
+      selected: () => i === this.props.selectedIndex(),
+      onSelect: () => this.getChangeAllowed(i) && this.props.setSelectedIndex(i),
+    }));
+  }
+
+  getChangeAllowed(i: number) {
+    const allowChange = this.props.allowChange;
+    return allowChange === undefined || allowChange() === true || i === this.props.selectedIndex();
+  }
+}
