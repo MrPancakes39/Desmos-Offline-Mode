30d59d81065ec2de180c4bdaffc6a0f7b7095547
MrPancakes39 <salmannhassan39@gmail.com>
2025-03-23 12:25:36 +0200

    Switching from subpath imports to path aliasing

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import path from "path";
+
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
